314bc8b8c80ac33825ee9389e7e1a10cd330a13e
Aviv Hakmon <avivhkn@cs.colman.ac.il>
2020-03-31 23:39:52 +0300

    adding some new icons

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react';
 import { inject, observer } from "mobx-react";
 import "../../styles/user.css"
-
+import movie from "./icons/movie.svg"
+import cigarette from "./icons/cigarette.svg"
+import beer from "./icons/beer.svg"
+import coffee from "./icons/coffee.svg"
+import dog from "./icons/dog.svg"
+import sport from "./icons/sport.svg"
+import message from "./icons/message.svg"
+import sos from "./icons/sos.svg"
 
 @inject('usersStore')
 @observer
 class User extends Component {
+    iconHandler(user) {
+        switch (user.mode) {
+            case "cigarette":
+                return (
+                    <img src={cigarette} className="cigarette icon" alt={"cigarette"} />
+                )
+            case "movie":
+                return (
+
+                    <img src={movie} className="movie icon" alt={"movie"} />
+                )
+            case "beer":
+                return (
+                    <img src={beer} className="beer icon" alt={"beer"} />
+                )
+            case "coffee":
+                return (
+                    <img src={coffee} className="coffee icon" alt={"coffee"} />
+                )
+            case "dog":
+                return (
+                    <img src={dog} className="dog icon" alt={"dog"} />
+                )
+            case "sos":
+                return (
+                    <img src={sos} className="sos icon" alt={"sos"} />
+                )
+            case "sport":
+                return (
+                    <img src={sport} className="sport icon" alt={"sport"} />
+                )
+            default:
+                return (
+                    <img src={message} className="message icon" alt={"message"} />
+                )
+        }
+    }
+
     render() {
         const user = this.props.user
 
@@ -13,7 +58,7 @@ class User extends Component {
             <div className="bigUser">
                 <div className="user">
                     <div className="person-name" className="fullName">{user.first_name + ' ' + user.last_name}</div>
-                    <div className="mode" className={user.mode}></div>
+                    {this.iconHandler(user)}
                     <div className="distance"> 78KM</div>
                 </div>
                 <div className="vl"></div>
