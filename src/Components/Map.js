a9f866fd75e89fb044d42dfecd3413e03fe25567
OhadGilboa <ogilboa3@gmail.com>
2020-04-02 23:50:19 +0300

    googleMaps

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-class Map extends Component {
+
+const mapStyles = {
+  width: '50%',
+  height: '50%',
+};
+
+class MapContainer extends Component {
 
   render() {
-    return <div className="map-comp">GOOGLE MAPS!!!</div>;
+    return(
+    <Map
+          google={this.props.google}
+          zoom={15}
+          style={mapStyles}
+          initialCenter={{ lat: 32.083543, lng: 34.793194}}
+        />
+    )
   }
 }
-export default Map;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyB3UHM8Nv2LOrnh7k7zLfA1h4js0uEIHuY'
+})(MapContainer);
