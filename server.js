1f6ddca70fef8fd7305a8b70e5bf6f41fcd97a54
Aviv Hakmon <avivhkn@cs.colman.ac.il>
2020-04-06 14:20:19 +0300

    nothing

--- server.js
+++ server.js
@@ -12,23 +12,24 @@ app.use(bodyParser.urlencoded({
 }))
 
 /////////////////////////////////////////////////////////////////////////////
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 
 /////////////////////////////////////////////////////////////////////////////
-// app.use(function (req, res, next) {
-    //     res.header('Access-Control-Allow-Origin', '*')
-    //     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    //     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    //     next()
-    // })
-    
-    
-    app.use('/', api)
-    
-    app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'build', 'index.html'));
-    });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    next()
+})
+
+
+app.use('/', api)
+//////////////////////////////////////////////////////////////////////////// 
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+//////////////////////////////////////////////////////////////////////////// 
 
 const port = 4200
 app.listen(process.env.PORT || port, function () {
