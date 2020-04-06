8e5c7d2bf635a32a01f81ecbb2e28ddf22eb030c
Aviv Hakmon <avivhkn@cs.colman.ac.il>
2020-04-06 09:39:43 +0300

    heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 }))
 
 /////////////////////////////////////////////////////////////////////////////
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
