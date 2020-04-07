e92b373f2b26269a3bdd336e3bed97f068c20686
Aviv Hakmon <avivhkn@cs.colman.ac.il>
2020-04-07 11:26:58 +0300

    dev mode

--- server.js
+++ server.js
@@ -19,21 +19,15 @@ app.use(function (req, res, next) {
 })
 
 /////////////////////////////////////////////////////////////////////////////
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 /////////////////////////////////////////////////////////////////////////////
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()
-})
 
 app.use('/', api)
 //////////////////////////////////////////////////////////////////////////// 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 //////////////////////////////////////////////////////////////////////////// 
 
 
