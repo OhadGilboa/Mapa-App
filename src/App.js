8a2f2a523be3f26acf1cddf0e9a91124cda088fa
Aviv Hakmon <avivhkn@cs.colman.ac.il>
2020-04-07 13:55:19 +0300

    test

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
             <Route path="/Messages" exact render={() => <div> {this.props.userData.user.first_name ? <Conversations /> : <Redirect to="/" />}  </div>} />
             <Route path="/Profile" exact render={() => <div className="userProfileApp"> {this.props.userData.user.first_name ? <UserProfile /> : <Redirect to="/" />} </div>} />
           </div>
-          <div  className={this.props.userData.user.userId ? "navbar" : null}>
+          <div className="navbar">
             {this.props.userData.user.first_name ? <Navbar /> : null}
           </div>
         </div>
